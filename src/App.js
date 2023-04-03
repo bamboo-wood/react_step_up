5146af8e0492a416c4a16274954ccead1fb3e86e
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-03 19:15:33 +0900

    delete css components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import { CssModules } from "./components/CssModules";
-import { InlineStyle } from "./components/InlineStyle";
-import { StyledJsx } from "./components/StyledJsx";
-import { StyledConponents } from "./components/StyledComponents";
-import { Emotion } from "./components/Emotion";
-import "./styles.css";
-
 export default function App() {
   return (
     <>
-      <div className="App">
-        <InlineStyle />
-        <CssModules />
-        <StyledJsx />
-        <StyledConponents />
-        <Emotion />
-      </div>
+      <div className="App"></div>
     </>
   );
 }
