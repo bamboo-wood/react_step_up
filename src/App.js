a45b4e774723881a38e3e2ebae7f5f2708f72b5d
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-03 15:47:48 +0900

    install sass

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import { useCallback, useState, useMemo } from "react";
-import { ChildArea } from "./components/ChildArea";
+import { InlineStyle } from "./components/InlineStyle";
+import "./styles.css";
 
 export default function App() {
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => setText(e.target.value);
-
-  const onClickOpen = () => setOpen(!open);
-
-  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
-
-  const temp = useMemo(() => {
-    console.log("usememo");
-    return 1 + 3;
-  }, []);
-  console.log(temp);
-
   return (
     <>
       <div className="App">
-        <input type="text" value={text} onChange={onChangeText} />
-        <br />
-        <br />
-        <button onClick={onClickOpen}>表示</button>
-        <ChildArea open={open} onClickClose={onClickClose} />
+        <InlineStyle />
       </div>
     </>
   );
