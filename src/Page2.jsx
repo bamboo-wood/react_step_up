dab77a8efa88519704890910aacb0b2a345f29d9
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-04 12:48:29 +0900

    useLocation

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -5,7 +5,9 @@ export const Page2 = () => {
   return (
     <div>
       <h1>Page1</h1>
-      <Link to="bamboo_wood">useParams</Link>
+      <Link to="bamboo_wood">URL Parameter</Link>
+      <br />
+      <Link to="bamboo_wood?name=hogehoge">Query Parameter</Link>
       <Outlet />
     </div>
   );
